b8bd3a733787a9066c249c70f33b88e791b63c75
lavender151 <trinhtan2000@gmail.com>
2022-05-24 21:53:18 +0700

    commit api download image

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -8,7 +8,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT || 8080;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
@@ -26,7 +26,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
+  app.get("/filteredimage", async (req, res) => {
+    var url = req.query.image_url;
+    let myFunc = async (data: any, func: any) => {
+      try{
+        let allFile = [];
+        const path = await filterImageFromURL(data);
+        allFile.push(path);
+        func(allFile);
+        res.status(200).send("success");
+      }catch(err){
+        res.status(422);
+        res.send(err);
+      }
+    }
+    if (url.match(/\.jpg$/) != null){
+      await myFunc(url, deleteLocalFiles);
+    }
+  });
   /**************************************************************************** */
 
   //! END @TODO1
